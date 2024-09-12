6e0ec48c5c0072fb6a30b6e97eac45828541291f
vrkolur <varunkolur17.com>
2024-09-12 19:51:36 +0530

    Added Image Controller

--- src/main/java/com/varun/shopping/controller/ImageController.java
+++ src/main/java/com/varun/shopping/controller/ImageController.java
@@ -1,17 +1,20 @@
 package com.varun.shopping.controller;
 
 import com.varun.shopping.dto.ImageDto;
+import com.varun.shopping.exception.ResourceNotFoundException;
+import com.varun.shopping.model.Image;
 import com.varun.shopping.response.ApiResponse;
 import com.varun.shopping.service.image.IImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -35,6 +38,43 @@ public class ImageController {
         }
     }
 
+    @PostMapping("/download/{id}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Integer id) {
+        Image image = imageService.getImageById(id);
+        try {
+            ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(image.getFileType()))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+                    .body(resource);
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponse> updateImage(@PathVariable Integer id, @RequestBody MultipartFile file) {
+        Image image = imageService.getImageById(id);
+        try {
+            if(image != null) {
+                imageService.updateImage(file, id);
+                return ResponseEntity.ok(new ApiResponse("Image updated successfully ", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Something went wrong ", null));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Integer id) {
+        try {
+            imageService.deleteImageById(id);
+            return ResponseEntity.ok(new ApiResponse("Image deleted successfully ", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 
 
 
